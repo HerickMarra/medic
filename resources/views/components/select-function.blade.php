3c93b788c772ab02e6693f1d88859fde814ec035
Herick <herick.costa@consultmidia.com.br>
2024-11-27 18:54:38 -0300

    *

--- resources/views/components/select-function.blade.php
+++ resources/views/components/select-function.blade.php
@@ -1,6 +1,10 @@
 <div class="x-select-function" id="x-select-function">
       
-    
+    <div class="x-select-function-sintomas">
+        <div class="x-select-function-sintomas-card">Febre</div>
+        <div class="x-select-function-sintomas-card">Dor de cabeça</div>
+        <div class="x-select-function-sintomas-card"> Gripe</div>
+    </div>
 
     <div class="x-select-function-desc" >
         <div class="x-select-function-desc-btn">Descrever sintomas (por texto)</div>
